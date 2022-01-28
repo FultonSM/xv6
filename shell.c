b7050869136cad4836c79f7f5b395c6ccfc49c56
Stephen Fulton <smfulton@mail.lipscomb.edu>
2022-01-28 13:14:44 -0600

    parse 'works', don't know how to feed it a line that will be turned into a command

--- shell.c
+++ shell.c
@@ -1,17 +1,23 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
-//#include "parse.h"
+#include "parse.h"
 
 int main(){
     //using shell to copy echo using read
-    char* cptr = malloc(sizeof(char)*10);
-    int gud = read(0,cptr,10);
+    char* cptr = malloc(sizeof(char)*12);
+    int gud = read(0,cptr,12);
     if(gud < 0){
-        printf(2,"error, idk\n");
+        printf(2,"error: read error\n");
         exit();
     }
-    printf(1,cptr);
-    printf(1,"\n\n");
+    command cmd;
+    printf(1,"got it, :%s\n",cptr);
+    gud = parse(cptr,&cmd);
+    if(gud == -1){
+        printf(2,"error: parse error\n");
+        exit();
+    }
+    printf(1,"bg = %d\nargc = %d\n");
     exit();
 }
