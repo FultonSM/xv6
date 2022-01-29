0613c37b81d638f94fa89a5ce98dccbbecdcdbeb
Stephen Fulton <smfulton@mail.lipscomb.edu>
2022-01-29 11:03:07 -0600

    shell now takes a string and executes it, now need to fork and read

--- shell.c
+++ shell.c
@@ -5,16 +5,24 @@
 
 int main(){
     //using shell to copy echo using read
-    char* cptr = "hello there > world";
+    char* cptr = "mem A";
     command cmd;
     int gud = parse(cptr,&cmd);
     if(gud == -1){
         printf(2,"Parse Error\n");
     }
+
+    //* print out cmd code
     printf(1,"bg: %d\nargc: %d\n",cmd.bg,cmd.argc);
     for(int i=0; i<cmd.argc;++i){
         printf(1,"argv[%d]: %s\n",i,cmd.argv[i]);
     }
     printf(1,"input: %s\noutput: %s\n",cmd.input,cmd.output);
+    //*/
+    
+    gud = exec(cmd.argv[0],cmd.argv);
+    if(gud == -1){
+        printf(2,"exec Error\n");
+    }
     exit();
 }
