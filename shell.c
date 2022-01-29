3770d6e931be1be146432bd5e73ecd6bf6ee088c
Stephen Fulton <smfulton@mail.lipscomb.edu>
2022-01-29 15:52:00 -0600

    works for no input commands

--- shell.c
+++ shell.c
@@ -4,43 +4,45 @@
 #include "parse.h"
 
 int main(){
-    //using shell to copy echo using read
-    char* cptr = "";
-    int gud = read(0,cptr,10);
-    if(gud < 0){
-        printf(2,"read Error\n");
-        exit();
-    }
-    //printf(1,"%s\n",cptr);
+    char *cprt = "";
+    char estr[] = "exit";
+    int gud = 0;
     command cmd;
-    gud = parse(cptr,&cmd);
-    if(gud == -1){
-        printf(2,"Parse Error\n");
-        exit();
-    }
+    while(1){
+        gud = read(0,cprt,8);
+        if(gud == -1){
+            printf(2,"Error: read error\n");
+            exit();
+        }
+        gud = parse(cprt,&cmd);
+        if(gud == -1){
+            printf(2,"Error: parse error\n");
+            exit();
+        }
+        if(strcmp(estr,cmd.argv[0]) == 0){
+            exit();
+        }
 
-    /* print out cmd code
-    printf(1,"bg: %d\nargc: %d\n",cmd.bg,cmd.argc);
-    for(int i=0; i<cmd.argc;++i){
-        printf(1,"argv[%d]: %s\n",i,cmd.argv[i]);
-    }
-    printf(1,"input: %s\noutput: %s\n",cmd.input,cmd.output);
-    //*/
+        
 
-    //fork code
-    int pd = fork();
-    if(pd < 0){
-        printf(2,"Fork Error\n");
+        int forkch = fork();
+        if(forkch == 0){
+            gud = exec(cmd.argv[0],cmd.argv);
+            if(gud == -1){
+                printf(2,"Error: exec error\n");
+            }
+        }
+        else if(forkch == -1){
+            printf(2,"Error: fork error\n");
+            exit();
+        }
+        else{
+            gud = wait();
+            if(gud == -1){
+                printf(2,"Error: wait error\n");
+            }
+        }
+
+        sleep(50);
     }
-    else if(pd == 0){
-        gud = exec(cmd.argv[0],cmd.argv);
-    }
-    else{
-        wait();
-    }
-    
-    if(gud == -1){
-        printf(2,"exec Error\n");
-    }
-    exit();
 }
