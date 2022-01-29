189437e5b163302bcbff94043d3750be86ceb45b
Stephen Fulton <smfulton@mail.lipscomb.edu>
2022-01-29 17:16:06 -0600

    fork loop works, now need background programs

--- shell.c
+++ shell.c
@@ -2,47 +2,67 @@
 #include "stat.h"
 #include "user.h"
 #include "parse.h"
+#include "fcntl.h"
 
 int main(){
-    char *cprt = "";
-    char estr[] = "exit";
+    char* cptr = malloc(1024);
     int gud = 0;
-    command cmd;
-    while(1){
-        gud = read(0,cprt,8);
-        if(gud == -1){
+
+    while(1){    
+        gud = read(0,cptr,1024);    //read line
+
+        //read error check
+        if(gud < 0){
             printf(2,"Error: read error\n");
             exit();
         }
-        gud = parse(cprt,&cmd);
-        if(gud == -1){
-            printf(2,"Error: parse error\n");
-            exit();
-        }
-        if(strcmp(estr,cmd.argv[0]) == 0){
-            exit();
-        }
-
         
+        //parsing the line
+        command cmd;
+        gud = parse(cptr,&cmd);
 
-        int forkch = fork();
-        if(forkch == 0){
-            gud = exec(cmd.argv[0],cmd.argv);
-            if(gud == -1){
-                printf(2,"Error: exec error\n");
-            }
+        //parse error check
+        if(gud == -1){
+            printf(2,"error: parse error\n");
+            exit();
         }
-        else if(forkch == -1){
+
+        //check if command is exit
+        if(strcmp(cmd.argv[0],"exit") == 0){
+            break;
+        }
+        /*
+        printf(1,"bg: %d\nargc: %d\n",cmd.bg,cmd.argc);
+        for(int i=0; i<cmd.argc;++i){
+            printf(1,"argv[%d]: %s\n",i,cmd.argv[i]);
+        }
+        printf(1,"input: %s\noutput: %s\n\n",cmd.input,cmd.output);
+        //*/
+
+        //calling fork
+        gud = fork();
+
+        //fork error check
+        if(gud == -1){
             printf(2,"Error: fork error\n");
             exit();
         }
-        else{
+        
+        //fork child check
+        if(gud > 0){
             gud = wait();
+            //wait error check
             if(gud == -1){
                 printf(2,"Error: wait error\n");
+                exit();
             }
         }
-
-        sleep(50);
+        else{
+            //if child, execute process
+            gud = exec(cmd.argv[0],cmd.argv);
+            exit();
+        }
     }
+
+    exit();
 }
