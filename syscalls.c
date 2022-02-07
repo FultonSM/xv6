e90e86c39846960dca7c9cf5cfd1ba5f75ea2cbb
Stephen Fulton <smfulton@mail.lipscomb.edu>
2022-02-07 17:04:41 -0600

    it works

--- syscalls.c
+++ syscalls.c
@@ -1,18 +1,30 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "spinlock.h"
 
-int main(){
-    printf(1,"This be the syscall function matey\n");
-    sleep(10);
-    sleep(10);
-    int pid = getpid();
-    int gud = calls(pid);
-    if(gud == -1){
-        printf(2,"Error: calls error");
+int main(int argc, char *argv[]){
+    //printf(1,"This be the syscall function matey\n");
+    if(argc > 2){
+        printf(1,"usage: syscalls <pid>\n");
+        return 0;
+    }
+    else if(argc == 2){
+        int pid = atoi(argv[1]);
+        int syscalls = calls(pid);
+        if(syscalls == -1){
+            printf(2,"Error: process does not exist\n");
+            exit();
+        }
+        printf(1,"%d\n",syscalls);
     }
     else{
-        printf(1,"%d\n");
+        for(int i=1;i<=1000;++i){
+            int syscalls = calls(i);
+            if(syscalls != -1){
+                printf(1,"%d: %d\n",i,syscalls);
+            }
+        }
     }
     exit();
 }
